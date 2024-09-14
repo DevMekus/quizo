754efc817a5e3e4ff6ac992aa15fb0c1ed6f7a23
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-09-14 07:43:20 +0100

    Admin dashboard sidebar linked

--- admin/index.php
+++ admin/index.php
@@ -6,6 +6,10 @@ include("header.php");
     <?php include("sidebar.php"); ?>
     <div class="page-content">
         <?php include("navbar.php"); ?>
+        <section class="content-centered">
+            <h3 class="page-title">Welcome back Impruuv Systems</h3>
+            <p class="page-description">Attend to the quiz overviews</p>
+        </section>
     </div>
 </main>
 <?php include("footer.php"); ?>
--- dashboard/index.php
+++ dashboard/index.php
@@ -6,6 +6,9 @@ include("header.php");
     <?php include("sidebar.php"); ?>
     <div class="page-content">
         <?php include("navbar.php"); ?>
+        <section class="content-centered">
+            <h5>Welcome back Impruuv Systems</h5>
+        </section>
     </div>
 </main>
 <?php include("footer.php"); ?>
