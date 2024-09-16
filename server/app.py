13313f9c2222814b05bcee86a3cdc2d0797185e3
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-09-16 10:12:22 +0100

    Entry point

--- server/app.py
+++ server/app.py
@@ -1,1 +1,20 @@
-  # Main application entry point
+# Main application entry point
+from flask import Flask
+from db import  db
+from auth.auth_routes import auth_blueprint
+from users.user_routes import user_blueprint
+from admin.admin_routes import admin_blueprint
+from quizzes.quiz_routes import quiz_blueprint
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+db.init_app(app)
+
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+app.register_blueprint(user_blueprint, url_prefix='/users')
+app.register_blueprint(admin_blueprint, url_prefix='/admin')
+app.register_blueprint(quiz_blueprint, url_prefix='/quizzes')
+
+if __name__ == '__main__':
+  app.run(debug=True)
