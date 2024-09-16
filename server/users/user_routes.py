0a53ea680c8c4b1aea78ec1614994c63c63578b1
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-09-16 09:36:44 +0100

    user routes

--- server/users/user_routes.py
+++ server/users/user_routes.py
@@ -1,1 +1,7 @@
  # User endpoints (e.g., profile, quizzes taken)
+from flask import Blueprint
+from users.user_controller import get_profile, update_user
+
+user_blueprint = Blueprint('users', __name__)
+user_blueprint.route('/me', methods=['GET'])(get_profile)
+user_blueprint.route('/update', methods=['PATCH'])(update_user)
