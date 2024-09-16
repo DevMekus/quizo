6334561c1e1e759d87f0414d2a871dc061af92e2
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-09-16 08:33:48 +0100

    User Model

--- server/models.py
+++ server/models.py
@@ -1,1 +1,23 @@
  # Database models for User, Admin, Quiz, etc.
+from db import db
+from werkzeug.security import generate_password_hash, check_password_hash
+import datetime
+
+class UserModel(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(10), default='user')
+    
+    def set_password(self, password):
+        self.password = generate_password_hash(self.password, password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+
+    
