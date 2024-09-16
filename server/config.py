bd065e49d7098722baf25d9c12cfde7feaf8052b
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-09-16 08:24:54 +0100

    Application Config.py

--- server/config.py
+++ server/config.py
@@ -1,1 +1,7 @@
 # Configuration file (e.g., for database, secret keys)
+import os
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY') or 'supersecretkey'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///quiz_app.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
