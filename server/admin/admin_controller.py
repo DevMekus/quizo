1708117408fd78d1c911932a980397dd7c414711
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-09-16 10:05:46 +0100

    Admin controller and routes

--- server/admin/admin_controller.py
+++ server/admin/admin_controller.py
@@ -1,1 +1,9 @@
  # Admin business logic
+from flask import jsonify
+from auth.auth_controller import token_required
+
+@token_required
+def admin_only(current_user):
+    if current_user.role !='admin':
+        return jsonify({'message':'Admins only','status':'error'}),403
+    return jsonify({'message':'Welcome admin','status':'success'})
--- server/admin/admin_routes.py
+++ server/admin/admin_routes.py
@@ -1,1 +1,6 @@
- # Admin endpoints (e.g., quiz management)
+ # Admin endpoints
+from flask import Blueprint
+from admin.admin_controller import admin_only
+
+admin_blueprint = Blueprint('admin',__name__)
+admin_blueprint.route('/admin', methods=['GET'])(admin_only)
