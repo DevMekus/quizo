469ce931c6b994f9697954ab3527deb866fa6aa9
DevMekus <nnaji.nnaemeka712@gmail.com>
2024-09-16 09:32:24 +0100

    User controller function: get_profile

--- server/users/user_controller.py
+++ server/users/user_controller.py
@@ -1,1 +1,17 @@
  # User business logic
+from flask import jsonify
+from auth.auth_controller import token_required
+
+@token_required
+def get_profile(current_user):
+    return jsonify({
+        'id': current_user.id,
+        'username':current_user.username,
+        'email': current_user.email,
+        'role':current_user.role
+    })
+
+#Update user profile
+@token_required
+def update_user(current_user):
+    pass
